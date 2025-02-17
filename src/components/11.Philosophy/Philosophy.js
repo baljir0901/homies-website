97f6cc4dd05d4bc9f8a38720e27d42a049145b56
baljir <baljir0901@gmail.com>
2025-02-17 19:28:44 +0900

    hardcodr

--- src/components/11.Philosophy/Philosophy.js
+++ src/components/11.Philosophy/Philosophy.js
@@ -30,7 +30,7 @@ const Philosophy = () => {
             <h2>代表者挨拶</h2>
             <div className="ceo-profile">
               <img
-                src="/homies-website/assets/ceo.jpg"
+                src={`${process.env.PUBLIC_URL}/assets/ceo.jpg`}
                 alt="CEO"
                 className="ceo-image"
               />
